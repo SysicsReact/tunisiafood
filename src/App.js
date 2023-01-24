af915bb3edd3edff22c916988f7cd85853b88e66
hakim njim <37658409+RymHamdi@users.noreply.github.com>
2023-01-24 16:10:33 +0100

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Index from "./pages/Index";
 import Header from "./components/Header";
-import '../src/assets/css/home-classic.css';
-import '../src/assets/css/main.css';
 import './App.css';
 function App (){
    return(
