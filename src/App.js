a7058da788a4927352ac5fd871096091130f9cac
hakim njim <37658409+RymHamdi@users.noreply.github.com>
2023-02-14 09:20:17 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import Index from "./Pages/Index";
-import Login from "./Pages/Login";
-import Register from "./Pages/register";
-import Reset from "./Pages/resetPwd";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/register";
+import Reset from "./pages/resetPwd";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Cart from "./components/Cart";
-import Profile from "./Pages/Profile";
-import About from "./Pages/About";
-import Blog from "./Pages/Blog";
-import Shop from "./Pages/Shop";
-import AllProducts from "./Pages/AllProducts";
-import SingleProduct from "./Pages/SingleProduct";
-import MyProfile from "./Pages/MyProfile";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Shop from "./pages/Shop";
+import AllProducts from "./pages/AllProducts";
+import SingleProduct from "./pages/SingleProduct";
+import MyProfile from "./pages/MyProfile";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import React, { Component, useState } from 'react'
@@ -21,7 +21,7 @@ import { jQuerycode } from "./components/Jq.js";
 import Loader from "./components/loader/Loader";
 import Product from "./components/Product";
 import ShopProduct from "./components/product/ShopProduct";
-import Contact from "./Pages/Contact";
+import Contact from "./pages/Contact";
 let test = false;
 class App extends Component {
 
