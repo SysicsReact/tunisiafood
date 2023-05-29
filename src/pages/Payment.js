6a221b2aec96db101d69b0368a4706d9a4ed9ecf
Hakim Njim <njim.hakimnjim1992@gmail.com>
2023-05-29 14:36:29 +0100

    Fix Payment

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -90,8 +90,8 @@ function Payment() {
       const url = "https://api.konnect.network/api/v2/payments/init-payment";
       const requestBody = {
         receiverWalletId: "642a7d6c2e9c6ea045f6f07b",
-        token: "TND",
-        amount: 5000 ,
+        token: "EUR",
+        amount: priceFinal ,
         type: "immediate",
         lifespan: 10,
         feesIncluded: false,
