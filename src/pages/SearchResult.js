53ab78685dd0fd9f2d917d0cf195ed57b424d0b7
hakim njim <37658409+RymHamdi@users.noreply.github.com>
2023-08-24 17:51:56 +0100

    Last Updates

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -5,28 +5,25 @@ import { REMOVE_FROM_WISH, selectWishItems } from "../redux/slice/wishSlice";
 import { ADD_TO_CART } from "../redux/slice/cartSlice";
 import { Link } from "react-router-dom";
 
-const WishDetails = () => {
+const SearchResult = () => {
     const navigate = useNavigate();
     const wishItems = useSelector(selectWishItems);
     const dispatch = useDispatch();
+    const [searchResults, setSearchResults] = useState([]);
     const addToCart = (wish) => {
         dispatch(
-           
-
             ADD_TO_CART(wish),
-            
             );
-            dispatch(
-           
-                REMOVE_FROM_WISH(wish),
-                
-                
-                );
            };
-    const removeFromWish = (wish) => {
-        dispatch(
-            REMOVE_FROM_WISH(wish));
-    }
+
+    useEffect(() => {
+      // Retrieve the stored result array from local storage
+      const storedResults = localStorage.getItem('searchResults');
+  
+      if (storedResults) {
+        setSearchResults(JSON.parse(storedResults));
+      }
+    }, []);
 
     return (
         <html lang="en">
@@ -54,57 +51,33 @@ const WishDetails = () => {
                 <a className="backtop fas fa-arrow-up" href="#"></a>
                 <section className="inner-section single-banner" style={{ backgroundImage: "url(assets/images/spices.jpg)", backgroundRepeat: "no-repeat", backgroundPosition: "center", }}>
                     <div className="container">
-                        <h2>Votre Liste des souhaits</h2>
+                        <h2>Résultat de recherche</h2>
                         <ol className="breadcrumb">
                             <li className="breadcrumb-item"><Link to="/">Accueil</Link></li>
-                            <li className="breadcrumb-item active" aria-current="page">Liste des souhaits</li>
+                            <li className="breadcrumb-item active" aria-current="page">Résultat de recherche</li>
                         </ol>
                     </div>
                 </section>
 
-            {wishItems.length === 0 ? (
-                <>
-                    <div className="cart-info-group">
-                        <div className="cart-footer">
-                            <h6 className="coupon-btn">Votre Liste Des Souhaits Est Vide !</h6>
-                        </div>
-                    </div>
-                    <div className="cart-footer">
-                        <a className="cart-checkout-btn" href="#">
-                           <Link to={"/ProductShop"}> <span className="checkout-label">Parcourir les produits</span></Link>
-                            <span className="checkout-price"><i className="fas fa-shopping-basket"></i></span>
-                        </a>
-                    </div>
-                </>
-            ) : (
-                <>
-                      
+   
              <section className="inner-section checkout-part">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <div className="alert-info">
-                            <p> <Link to="/ShopProduct"><a href="">Retour aux achats ?</a></Link></p>
-                        </div>
-                    </div>
-                    <div className="col-lg-12">
                         <div className="account-card">
-                            <div className="account-title">
-                                <h4>Votre liste de souhaits</h4>
-                            </div>
                             <div className="account-content">
                                 <div className="table-scroll">
                                     <table className="table-list">
                                     <thead>
                                     <tr>
-                                        <th scope="col">Product</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Price</th>
-                                        <th scope="col">shopping</th>
-                                        <th scope="col">action</th>
+                                        <th scope="col">Produit</th>
+                                        <th scope="col">Nom</th>
+                                        <th scope="col">Prix</th>
+                                        <th scope="col">Acheter</th>
+                                     
                                     </tr>
                                 </thead>
-                         {wishItems.map((wish, index) => {
+                                {searchResults.map((wish, index) => {
                             const { id, name, price, photo,} = wish;
                             return (
                                 <>
@@ -112,18 +85,15 @@ const WishDetails = () => {
                                             <tr>
                                         <td className="table-image"><img src={photo} alt="product"/></td>
                                         <td className="table-name"><h6>{name}</h6></td>
-                                        <td className="table-price"><h6>${price}<small>/kilo</small></h6></td>
+                                        <td className="table-price"><h6>${price}</h6></td>
                                         <td className="table-shop">
                                             <button className="product-add" title="Ajouter" onClick={() => addToCart(wish)} >Ajouter</button>
                                         </td>
-                                        <td className="table-action">
-                                            <a className="trash"  title="Supprimer" onClick={() => removeFromWish(wish)}><i className="icofont-trash"></i></a>
-                                        </td>
+                                        
                                     </tr>
                                         </tbody>
                                 </>)
                         })}
-
                                     </table>
                                 </div>
                             </div>
@@ -134,12 +104,9 @@ const WishDetails = () => {
                 
             </div>
                </section>
-                                
-
-                </>)
-            }
+               
         </body>
         </html>
     )
 }
-export default WishDetails;
+export default SearchResult;
